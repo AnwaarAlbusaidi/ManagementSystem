3d288ea55e937223e1e734e8dd095db554aeabd4
AnwaarAlbusaidi <AnwaarAlbusaidi5@gmail.com>
2022-12-07 21:17:55 -0800

    School management system

---
+++ src/SchoolManagment.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author LAP-8
+ *
+ */
+public class SchoolManagment {
+
+	/**
+	 * This class we will manage school system
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
